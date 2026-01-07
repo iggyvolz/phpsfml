70202430c9c319c8c267c25c729cb863f31554e6
Katie Volz <iggyvolz@gmail.com>
2026-01-06 22:33:57 -0500

    Add variant storage retrievers

--- ext/util.hpp
+++ ext/util.hpp
@@ -1,8 +1,10 @@
 #pragma once
-#include <map>
 #include <php.h>
 #include <string>
 #include <typeinfo>
+#include <variant>
+
+#include "util.hpp"
 
 void storage_put(uint32_t object_id, const std::type_info& type, void* data);
 void storage_put(uint32_t object_id, const std::type_info& type, const void* data);
@@ -45,17 +47,44 @@ template<typename T>
 T* storage_get(uint32_t object_id) {
     return static_cast<T*>(storage_get(object_id, typeid(T)));
 }
+template<typename T1, typename T2, typename... Values>
+std::variant<std::nullptr_t, T1*, T2*, Values*...> storage_get(uint32_t object_id) {
+    std::variant<std::nullptr_t, T1*, T2*, Values*...> result = nullptr;
+    auto* t1 = storage_get<T1>(object_id);
+    if (t1 != nullptr) result = t1;
+    auto* t2 = storage_get<T1>(object_id);
+    if (t2 != nullptr) result = t2;
+    ([&result, object_id] {
+        auto* ptr = storage_get<Values>(object_id);
+        if (ptr != nullptr) {
+            result = ptr;
+        }
+    }(), ...);
+    return result;
+}
 template<typename T>
 T* storage_get(zval zval) {
     return storage_get<T>(Z_OBJ_HANDLE(zval));
+}
+template<typename T1, typename T2, typename... Values>
+std::variant<std::nullptr_t, T1*, T2*, Values*...> storage_get(zval zval) {
+    return storage_get<T1, T2, Values...>(Z_OBJ_HANDLE(zval));
 }
 template<typename T>
 T* storage_get(zval* zval) {
     return storage_get<T>(Z_OBJ_HANDLE_P(zval));
 }
+template<typename T1, typename T2, typename... Values>
+std::variant<std::nullptr_t, T1*, T2*, Values*...> storage_get(zval* zval) {
+    return storage_get<T1, T2, Values...>(Z_OBJ_HANDLE_P(zval));
+}
 template<typename T>
 T* storage_get(zend_execute_data* execute_data) {
     return storage_get<T>(execute_data->This);
+}
+template<typename T1, typename T2, typename... Values>
+std::variant<std::nullptr_t, T1*, T2*, Values*...> storage_get(zend_execute_data* execute_data) {
+    return storage_get<T1, T2, Values...>(execute_data->This);
 }
 template<typename T>
 void storage_remove(uint32_t object_id) {
